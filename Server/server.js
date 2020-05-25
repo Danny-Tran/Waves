db900ae031373392a3653db571a25292208b3230
Danny-Tran <truong_tran@live.com>
2020-05-25 15:42:07 -0600

    testing connection 3000

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`Server Running at ${port}`)
