ff6e15f11dc1d27f40dde05ffa7f670cd8d801d0
Danny-Tran <truong_tran@live.com>
2020-05-27 16:21:32 -0600

    add user schema

--- Server/models/user.js
+++ Server/models/user.js
@@ -1,7 +1,39 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: 1
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    lastname:{
+        type: String,
+        required: true,
+    },
+    cart:{
+        type: Array,
+        default: []
+    },
+    history:{
+        type: Array,
+        default: []
+    },
+    role:{
+        type: Number,
+        default: 0
+    },
+    token:{
+        type: String,
+    }
 })
 
 const User = mongoose.model('User',userSchema);
