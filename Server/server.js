944908c1b28a4bd9c6781fc8049e038eedf19203
Danny-Tran <truong_tran@live.com>
2020-05-27 11:41:45 -0600

    app now connect to mongodb, complete evironment marblefor app

--- Server/server.js
+++ Server/server.js
@@ -2,8 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-
 const app = express();
+
+require ('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
